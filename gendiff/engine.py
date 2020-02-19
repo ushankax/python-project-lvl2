93d4ded18f260a5a791a2244a400174fdae58bc8
ushankax <ushanka.x@gmail.com>
2020-02-19 23:29:40 +0300

    deep diff

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,21 +1,48 @@
 import json
 
 
+d = []
+
+
+def diff_deeper(first_file, second_file):
+    for first, second in zip(first_file.items(), first_file.items()):
+        k1, v1 = first
+        k2, v2 = second
+        if k1 == k2 and isinstance(v1, dict) and isinstance(v2, dict):
+            d.append('{{{}: '.format(k1))
+            diff_deeper(v1, v2)
+            d.append('}')
+        else:
+            d.append('{{+ {}: {}, - {}: {}}}'.format(k2, v2, k1, v1))
+
+    return "".join(d)
+
+
 def generate_diff(first_file, second_file):
     first_file = json.load(open('gendiff/files/before.json'))
-    second_file =json.load(open('gendiff/files/after.json'))
-    d = {}
+    second_file = json.load(open('gendiff/files/after.json'))
+    equal = first_file.keys() & second_file.keys()
+    added = first_file.keys() - second_file.keys()
+    deleted = second_file.keys() - first_file.keys()
+    result = []
 
-    for item in first_file.keys():
-        if item in second_file.keys():
-            if first_file[item] == second_file[item]:
-                d["{}".format(item)] = first_file[item]
-            else:
-                d["+ {}".format(item)] = first_file[item]
-                d["- {}".format(item)] = second_file[item]
-        elif item not in second_file.keys():
-            d["+ {}".format(item)] = first_file[item]
-    
-    da = json.dumps(d, indent=4)
+    result.append("{")
 
-    return da
+    for item in equal:
+        if isinstance(first_file[item], dict) and isinstance(second_file[item], dict):
+            result.append("  {}: {}".format(item, diff_deeper(first_file[item], second_file[item])))
+        elif first_file[item] == second_file[item]:
+            result.append("  {}: {}".format(item, first_file[item]))
+        else:
+            result.append("+ {}: {}".format(item, second_file[item]))
+            result.append("- {}: {}".format(item, first_file[item]))
+
+    for item in added:
+        result.append("+ {}: {}".format(item, first_file[item]))
+
+    for item in deleted:
+        result.append("- {}: {}".format(item, second_file[item]))
+
+    result.append("}")
+
+    return "\n".join(result)
