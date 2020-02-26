c4855f8700978b7a6afa7ac783c1d0e68a2af097
ushankax <ushanka.x@gmail.com>
2020-02-26 23:18:05 +0300

    added relative dir work

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,47 +1,48 @@
 import json
-
+import os
 
 d = []
 
 
-def diff_deeper(first_file, second_file):
-    for first, second in zip(first_file.items(), first_file.items()):
-        k1, v1 = first
-        k2, v2 = second
-        if k1 == k2 and isinstance(v1, dict) and isinstance(v2, dict):
-            d.append('{{{}: '.format(k1))
-            diff_deeper(v1, v2)
+def diff_deeper(old, new):
+    for old_items, new_items in zip(old.items(), old.items()):
+        old_k, old_v = old_items
+        new_k, new_v = new_items
+        if old_k == new_k and isinstance(new_v, dict):
+            d.append('{{{}: '.format(old_k))
+            diff_deeper(old_v, new_v)
             d.append('}')
         else:
-            d.append('{{+ {}: {}, - {}: {}}}'.format(k2, v2, k1, v1))
+            d.append('{{+ {}: {}, - {}: {}}}'.format(new_k, new_v, old_k, old_v))
 
     return "".join(d)
 
 
-def generate_diff(first_file, second_file):
-    first_file = json.load(open('gendiff/files/before.json'))
-    second_file = json.load(open('gendiff/files/after.json'))
-    equal = first_file.keys() & second_file.keys()
-    added = first_file.keys() - second_file.keys()
-    deleted = second_file.keys() - first_file.keys()
+def generate_diff(old, new):
+    os.chdir("gendiff/files/")
+    old = json.load(open(old))
+    new = json.load(open(new))
+    equal = old.keys() & new.keys()
+    added = old.keys() - new.keys()
+    deleted = new.keys() - old.keys()
     result = []
 
     result.append("{")
 
-    for item in equal:
-        if isinstance(first_file[item], dict) and isinstance(second_file[item], dict):
-            result.append("  {}: {}".format(item, diff_deeper(first_file[item], second_file[item])))
-        elif first_file[item] == second_file[item]:
-            result.append("  {}: {}".format(item, first_file[item]))
+    for k in equal:
+        if isinstance(new[k], dict):
+            result.append("  {}: {}".format(k, diff_deeper(old[k], new[k])))
+        elif old[k] == new[k]:
+            result.append("  {}: {}".format(k, old[k]))
         else:
-            result.append("+ {}: {}".format(item, second_file[item]))
-            result.append("- {}: {}".format(item, first_file[item]))
+            result.append("+ {}: {}".format(k, new[k]))
+            result.append("- {}: {}".format(k, old[k]))
 
-    for item in added:
-        result.append("+ {}: {}".format(item, first_file[item]))
+    for k in added:
+        result.append("+ {}: {}".format(k, old[k]))
 
-    for item in deleted:
-        result.append("- {}: {}".format(item, second_file[item]))
+    for k in deleted:
+        result.append("- {}: {}".format(k, new[k]))
 
     result.append("}")
 
